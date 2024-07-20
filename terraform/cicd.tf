79cfe4e0376e91c43fd9be6ee8e9997f753641ab
hitose kakazu <hi.ka1749@gmail.com>
2024-07-20 12:45:18 +0900

    [develop] 環境設定

--- terraform/cicd.tf
+++ terraform/cicd.tf
@@ -3,7 +3,7 @@
 ############################################
 module "codepipeline_s3" {
   source      = "./modules/aws_s3"
-  bucket_name = "${var.project_name}-codepipeline-test-bucket"
+  bucket_name = "${var.terraform_project_name}-codepipeline-test-bucket"
 }
 
 ############################################
@@ -11,7 +11,7 @@ module "codepipeline_s3" {
 ############################################
 module "codecommit" {
   source          = "./modules/aws_codecommit"
-  repository_name = var.project_name
+  repository_name = var.terraform_project_name
 }
 
 ############################################
@@ -19,13 +19,13 @@ module "codecommit" {
 ############################################
 module "codebuild_role" {
   source     = "./modules/aws_iam_role"
-  role_name  = "${var.project_name}-codebuild-role"
+  role_name  = "${var.terraform_project_name}-codebuild-role"
   service    = "codebuild.amazonaws.com"
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
 }
 module "codebuild" {
   source                 = "./modules/aws_codebuild"
-  codebuild_project_name = "${var.project_name}-terraform-build"
+  codebuild_project_name = "${var.terraform_project_name}-terraform-build"
   service_role           = module.codebuild_role.role_arn
 }
 
@@ -34,15 +34,15 @@ module "codebuild" {
 ############################################
 # module "codedeploy_role" {
 #   source     = "./modules/aws_iam_role"
-#   role_name  = "${var.project_name}-codedeploy-role"
+#   role_name  = "${var.terraform_project_name}-codedeploy-role"
 #   service    = "codedeploy.amazonaws.com"
 #   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
 # }
 
 # module "codedeploy" {
 #   source                = "./modules/aws_codedeploy"
-#   app_name              = "${var.project_name}-codedeploy-app"
-#   deployment_group_name = "${var.project_name}-deployment-group"
+#   app_name              = "${var.terraform_project_name}-codedeploy-app"
+#   deployment_group_name = "${var.terraform_project_name}-deployment-group"
 #   service_role          = module.codedeploy_role.role_arn
 # }
 
@@ -51,13 +51,13 @@ module "codebuild" {
 ############################################
 module "codepipeline_role" {
   source     = "./modules/aws_iam_role"
-  role_name  = "${var.project_name}-codepipeline-role"
+  role_name  = "${var.terraform_project_name}-codepipeline-role"
   service    = "codepipeline.amazonaws.com"
   policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
 }
 
 resource "aws_iam_role_policy" "codepipeline_codecommit_policy" {
-  name   = "${var.project_name}_codepipeline_codecommit_policy"
+  name   = "${var.terraform_project_name}_codepipeline_codecommit_policy"
   role   = module.codepipeline_role.role_name
   policy = <<POLICY
 {
@@ -73,7 +73,7 @@ resource "aws_iam_role_policy" "codepipeline_codecommit_policy" {
         "codecommit:BatchGetRepositories",
         "codecommit:GetRepositoryTriggers"
       ],
-      "Resource": "arn:aws:codecommit:${var.region}:${var.account_id}:${module.codecommit.repository_name}"
+      "Resource": "arn:aws:codecommit:${var.terraform_region}:${var.terraform_account_id}:${module.codecommit.repository_name}"
     }
   ]
 }
@@ -81,7 +81,7 @@ POLICY
 }
 module "codepipeline" {
   source            = "./modules/aws_codepipeline"
-  pipeline_name     = "${var.project_name}-pipeline"
+  pipeline_name     = "${var.terraform_project_name}-pipeline"
   codepipeline_role = module.codepipeline_role.role_arn
   artifact_store    = module.codepipeline_s3.bucket_name
   codecommit_repo   = module.codecommit.repository_name
--- terraform/ml_resources.tf
+++ terraform/ml_resources.tf
@@ -3,7 +3,7 @@
 ############################################
 # module "output_s3" {
 #   source      = "./modules/aws_s3"
-#   bucket_name = "${var.project_name}-databricks-output-test-bucket"
+#   bucket_name = "${var.terraform_project_name}-databricks-output-test-bucket"
 # }
 
 ############################################
@@ -11,28 +11,28 @@
 ############################################
 # module "databricks" {
 #   source           = "./modules/databricks"
-#   workspace_name   = "${var.project_name}-workspace"
-#   cluster_name     = "${var.project_name}-cluster"
+#   workspace_name   = "${var.terraform_project_name}-workspace"
+#   cluster_name     = "${var.terraform_project_name}-cluster"
 #   job_name         = "example-job"
 #   notebook_path    = "/Users/example@example.com/TestNotebook"
-#   databricks_host  = var.databricks_host
-#   databricks_token = var.databricks_token
+#   databricks_host  = var.terraform_databricks_host
+#   databricks_token = var.terraform_databricks_token
 # }
 
 # module "databricks_catalog" {
 #   source       = "./modules/databricks_catalog_schema_table"
-#   catalog_name = var.catalog_name
+#   catalog_name = var.terraform_catalog_name
 # }
 
 # module "databricks_schema" {
 #   source       = "./modules/databricks_catalog_schema_table"
-#   schema_name  = var.schema_name
+#   schema_name  = var.terraform_schema_name
 #   catalog_name = module.databricks_catalog.catalog_name
 # }
 
 # module "databricks_table" {
 #   source       = "./modules/databricks_catalog_schema_table"
-#   table_name   = var.table_name
+#   table_name   = var.terraform_table_name
 #   catalog_name = module.databricks_catalog.catalog_name
 #   schema_name  = module.databricks_schema.schema_name
 # }
